6d9945af491d8b18d524b9ee0df32d37f8f878e8
Lawon <disaster.archivist@gmail.com>
2023-01-13 14:53:21 +0900

    백준 Level2 Project

--- Assets/Scripts/C#/Study01/BAEKJOON/Program.cs
+++ Assets/Scripts/C#/Study01/BAEKJOON/Program.cs
@@ -10,8 +10,15 @@ namespace BAEKJOON
     {
         static void Main(string[] args)
         {
-            Level1 level1 = new Level1();
+            Program program = new Program();
+            //program.Lv1_Test();
+            program.Lv2_Test();
+        }
 
+        
+        public void Lv1_Test()
+        {
+            Level1 level1 = new Level1();
             //  level1.Q2257();     // OK
             //  level1.Q1000();     // OK
             //  level1.Q1001();     // OK
@@ -25,8 +32,20 @@ namespace BAEKJOON
             //  level1.Q2588();     // OK
             //  level1.Q10171();    // OK
             //  level1.Q10172();    // OK
-            level1.Q25083();    // OK
+            //  level1.Q25083();    // OK
+        }
 
+
+        public void Lv2_Test()
+        {
+            Level2 level2 = new Level2();
+            //  level2.Q1330();     // OK
+            //  level2.Q9498();     // OK
+            //  level2.Q2753();     // OK
+            //  level2.Q14681();    // OK
+            //  level2.Q2884();     // OK
+            //  level2.Q2525();     // OK
+            //  level2.Q2480();     // OK
         }
     }
 }
