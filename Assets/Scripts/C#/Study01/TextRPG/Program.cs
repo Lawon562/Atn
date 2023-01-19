b6a5aabd234034168e94b45639986ab6d0c2369f
Lawon <disaster.archivist@gmail.com>
2023-01-19 23:15:40 +0900

    TextRPG introScene.Play() / Exit() 추가

--- Assets/Scripts/C#/Study01/TextRPG/Program.cs
+++ Assets/Scripts/C#/Study01/TextRPG/Program.cs
@@ -4,31 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using TextRPG.Scenes;
 /*
- * 프로젝트 이름 : 
- * 개발 일자 : 2023.01.19
- * 프로젝트 설명 : Console로 플레이하는 Text RPG
- */
+* 프로젝트 이름 : 
+* 개발 일자 : 2023.01.19
+* 프로젝트 설명 : Console로 플레이하는 Text RPG
+*/
 
 namespace TextRPG
 {
     class Program
-    { 
-        static void Exit()
-        {
-            TextColor.Fore_White();
-            Console.Write("게임을 종료하시겠습니까?(yes를 입력하시면 게임이 종료됩니다) : ");
-            if (Console.ReadLine().Equals("yes")) return;
-            TextColor.ResetColor();
-        }
+    {
+        
+        
+       
 
+        //------------
         static void Main(string[] args)
         {
             Settings.Set();
-            //int select = TitleScene.Play();
+            while (Key.game)
+            {
+                Common.ResetScreen();
+                int select = TitleScene.Play();
+                //int select = 4;
 
-            
-
+                Console.Clear();
+                if (select == 1)
+                {
+                    IntroScene.Play();
+                }
+                else if (select == 4)
+                {
+                    Common.Exit();
+                }
+            }
         }
+        //------------
+        
+        
     }
 }
