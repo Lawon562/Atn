692049994f3f43c0e23c65cf29b8eb033533da86
Lawon <disaster.archivist@gmail.com>
2023-01-20 19:56:51 +0900

    TextRPG Potion 클래스 생성

---
+++ Assets/Scripts/C#/Study01/TextRPG/Object/Potion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG.Object
+{
+    public enum PotionType { Life, Mana, Stamina, None }
+
+    abstract class Potion
+    {
+        public PotionType type = PotionType.None;
+        public PotionType Type { get; set; }
+
+        private int amount = 0;
+        public int Amount { get; set; }
+        
+    }
+}
