eea54966392ad809daa3c02db3f263ef9e9a11d3
Lawon <disaster.archivist@gmail.com>
2023-01-20 19:52:40 +0900

    TextRPG Character -> Player/Monster class 생성

---
+++ Assets/Scripts/C#/Study01/TextRPG/Character/ICharacter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG.Character
+{
+    public interface ICharacter
+    {
+        int MaxLife { get; set; }
+        int MaxMana { get; set; }
+        int MaxStamina { get; set; }
+        int Life { get; set; }
+        int Mana { get; set; }
+        int Stamina { get; set; }
+        int Str { get; set; }
+        int Intellect { get; set; }
+        int Dex { get; set; }
+        int Will { get; set; }
+        int Luck { get; set; }
+        int DefPoint { get; set; }
+        int DefPercent { get; set; }
+        
+
+    }
+}
---
+++ Assets/Scripts/C#/Study01/TextRPG/Character/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextRPG.Object;
+
+namespace TextRPG.Character
+{
+    class Player : Character
+    {
+        private int weapon;
+        public Weapon Weapon { get; set; }
+
+        public void Attack(Monster Target)
+        {
+            int point;
+            try
+            {
+                point = (this.Str + this.weapon - Target.DefPoint) / Target.DefPercent;
+            }
+            catch (DivideByZeroException e)
+            {
+                // To-do Somthing
+            }
+            catch (Exception e)
+            {
+                // To-do Somthing
+            }
+            finally
+            {
+                point = 1;
+            }
+            Target.Life -= (point > 0) ? point : 1;
+        }
+
+        public int PotionAmountCalc(int max, int current, int amount)
+        {
+            if (max >= current + amount) current += amount;
+            else current = max;
+            return current;
+        }
+        public void Recovery(Potion potion)
+        {
+            switch(potion.Type)
+            {
+                case PotionType.Life:
+                    Life = PotionAmountCalc(MaxLife, Life, potion.Amount); break;
+                case PotionType.Mana:
+                    Mana = PotionAmountCalc(MaxMana, Mana, potion.Amount); break;
+                case PotionType.Stamina:
+                    Stamina = PotionAmountCalc(MaxStamina, Stamina, potion.Amount); break;
+                case PotionType.None:
+                    // Error
+                    break;
+            }
+        }
+
+    }
+}
