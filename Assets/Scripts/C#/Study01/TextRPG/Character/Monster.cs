1a1ccf65295a5a85517f2976d5ae624d883077ff
Lawon <disaster.archivist@gmail.com>
2023-01-27 16:24:19 +0900

    TextRPG Monster 생성자 추가, 공격 데미지 임시 수정

--- Assets/Scripts/C#/Study01/TextRPG/Character/Monster.cs
+++ Assets/Scripts/C#/Study01/TextRPG/Character/Monster.cs
@@ -10,26 +10,36 @@ namespace TextRPG.Character
     class Monster : Character
     {
 
+        public Monster(string name)
+        {
+            this.Color = ConsoleColor.Red;
+            this.Name = name;
+            this.Life = 30;
+            this.Str = 3;
+            this.DefPoint = 1;
+            this.DefPercent = 1;
+        }
         public void Attack(Player Target)
         {
-            int point;
+            int point = 0;
             try
             {
                 point = (this.Str - Target.DefPoint) / Target.DefPercent;
             }
             catch (DivideByZeroException e)
             {
-                // To-do Somthing
+                point = 1;
             }
             catch (Exception e)
             {
-                // To-do Somthing
+                point = 1;
             }
             finally
             {
-                point = 1;
+                
             }
             Target.Life -= (point > 0) ? point : 1;
+            if (Target.Life < 0) Target.Life = 0;
         }
     }
 }
